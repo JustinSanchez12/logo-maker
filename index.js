c44e1c4117cb4670fb5a1b53997df5e2b6e9549a
Justin Sanchez <justinlsanchez12@gmail.com>
2023-05-25 02:32:23 -0700

    updated index to now just call cli.js

--- index.js
+++ index.js
@@ -1,47 +1,5 @@
-//Node module packages needed for this application
-const inquirer = require("inquirer");
-const fs = require('fs');
+const CLI = require('./lib/cli.js');
 
+const cli = new CLI();
 
-const { writeFile } = require('fs').promises;
-
-const promptUser = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'chars',
-            message: 'Please type your three characters: '
-        },
-        {
-            type: 'input',
-            name: 'text_colors',
-            message: 'Please enter color or hexadecimal number for text: '
-        },
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'Please enter the shape: ',
-            choices: ['circle', 'triangle', 'square'],
-        },
-        {
-            type: 'input',
-            name: 'shape_colors',
-            message: 'Please enter color or hexadecimal number for shape: '
-        }
-    ]);
-};
-
-//Placeholder until generate the svg file
-promptUser();
-
-// TODO: Generate the SVG file called 'logo.svg'
-/*
-const init = () => {
-    promptUser()
-        .then((answers) => writeFile('logo.svg', generateSVG(answers)))
-        .then(() => console.log('Successfully generated logo.svg'))
-        .catch((err) => console.log(err));
-};
-
-init();*/
-
+cli.run();
